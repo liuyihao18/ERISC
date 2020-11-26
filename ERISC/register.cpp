2cae14bc9d51df32a3e58426c05b1094f16cb9cf
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-26 19:28:35 +0800

    完成setStatus

--- ERISC/register.cpp
+++ ERISC/register.cpp
@@ -9,35 +9,35 @@ static const std::string names[64]{
 	"x0", "zero",
 	"x1", "ra",
 	"x2", "sp",
-	"x3", "gp",
-	"x4", "tp",
-	"x5", "t0",
-	"x6", "t1",
-	"x7", "t2",
-	"x8", "fp",
-	"x9", "s1",
-	"x10","a0",
-	"x11","a1",
-	"x12","a2",
-	"x13","a3",
-	"x14","a4",
-	"x15","a5",
-	"x16","a6",
-	"x17","a7",
-	"x18","s2",
-	"x19","s3",
-	"x20","s4",
-	"x21","s5",
-	"x22","s6",
-	"x23","s7",
-	"x24","s8",
-	"x25","s9",
-	"x26","s10",
-	"x27","s11",
-	"x28","t3",
-	"x29","t4",
-	"x30","t5",
-	"x31","t6"
+"x3", "gp",
+"x4", "tp",
+"x5", "t0",
+"x6", "t1",
+"x7", "t2",
+"x8", "fp",
+"x9", "s1",
+"x10", "a0",
+"x11", "a1",
+"x12", "a2",
+"x13", "a3",
+"x14", "a4",
+"x15", "a5",
+"x16", "a6",
+"x17", "a7",
+"x18", "s2",
+"x19", "s3",
+"x20", "s4",
+"x21", "s5",
+"x22", "s6",
+"x23", "s7",
+"x24", "s8",
+"x25", "s9",
+"x26", "s10",
+"x27", "s11",
+"x28", "t3",
+"x29", "t4",
+"x30", "t5",
+"x31", "t6"
 };
 
 /**
@@ -106,7 +106,39 @@ void Register::reset() {
 * param status: ×´Ì¬
 */
 void Register::setStatus(const std::string& name, Status status) {
-	m_status[lookup(name)] = status;
+	int index = lookup(name);
+	switch (status) {
+	case WRITE:
+		switch (m_status[index]) {
+		case NO_OPERATE:
+			m_status[index] = WRITE;
+			break;
+		case READ:
+			m_status[index] = READ_WRITE;
+			break;
+		default:
+			break;
+		}
+		break;
+	case READ:
+		switch (m_status[index]) {
+		case NO_OPERATE:
+			m_status[index] = READ;
+			break;
+		case WRITE:
+			m_status[index] = READ_WRITE;
+			break;
+		default:
+			break;
+		}
+		break;
+	case READ_WRITE:
+		m_status[index] = READ_WRITE;
+		break;
+	default:
+		std::cerr << "¼Ä´æÆ÷×´Ì¬ÉèÖÃ´íÎó£¡" << std::endl;
+		exit(-1);
+	}
 }
 
 /**
