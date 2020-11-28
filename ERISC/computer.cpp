00db8bb11854b41045a3b8b092fee841e4dfc3e6
hangzhou188 <1162951519@qq.com>
2020-11-28 15:14:48 +0800

    test

--- ERISC/computer.cpp
+++ ERISC/computer.cpp
@@ -11,38 +11,80 @@ void Computer::main() {
 		{
 		case STORE:
 			m_memory.store(m_register[current_line.op1], m_register[current_line.op2]);
+			m_register.setStatus(current_line.op1, READ);
+			m_register.setStatus(current_line.op2, READ);
 			input.nextLine();
 			break;
 		case PUSH:
 			m_stack.push(m_register[current_line.op1]);
-			if(m_register[current_line.op1])
+			m_register.setStatus(current_line.op1, READ);
 			break;
 		case POP:
-			m_stack.pop(m_register[current_line.op2]);
+			m_stack.pop(m_register[current_line.op1]);
+			m_register.setStatus(current_line.op1,READ);
 			break;
 		case MOV:
 			m_register.mov(current_line.op1, current_line.op2);
+			m_register.setStatus(current_line.op1, WRITE);
+			if(current_line.op2[0]!='0'){
+				m_register.setStatus(current_line.op2, READ);
+			}
 			break;
 		case ADD:
 			m_register.add(current_line.op1, current_line.op2,current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case SUB:
 			m_register.sub(current_line.op1, current_line.op2,current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case MUL:
 			m_register.mul(current_line.op1, current_line.op2, current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case DIV:
 			m_register.div(current_line.op1, current_line.op2, current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case REM:
 			m_register.rem(current_line.op1, current_line.op2, current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case AND:
 			m_register.AND(current_line.op1, current_line.op2, current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case OR:
 			m_register.OR(current_line.op1, current_line.op2, current_line.op3);
+			m_register.setStatus(current_line.op1, WRITE);
+			m_register.setStatus(current_line.op2, READ);
+			if (current_line.op3[0] != '0') {
+				m_register.setStatus(current_line.op3, READ);
+			}
 			break;
 		case LINE_LABLE:
 			break;
@@ -50,27 +92,39 @@ void Computer::main() {
 			input.jumpLine(current_line.op1);
 			break;
 		case BEQ:
+			m_register.setStatus(current_line.op1, READ);
+			m_register.setStatus(current_line.op2, READ);
 			if (m_register.beq(current_line.op1, current_line.op2)) {
 				input.jumpLine(current_line.op3);
 			}
 			break;
 		case BNE:
+			m_register.setStatus(current_line.op1, READ);
+			m_register.setStatus(current_line.op2, READ);
 			if (m_register.bne(current_line.op1, current_line.op2)) {
 				input.jumpLine(current_line.op3);
 			}
 			break;
 		case BLT:
+			m_register.setStatus(current_line.op1, READ);
+			m_register.setStatus(current_line.op2, READ);
 			if (m_register.blt(current_line.op1, current_line.op2)) {
 				input.jumpLine(current_line.op3);
 			}
 			break;
 		case BGE:
+			m_register.setStatus(current_line.op1, READ);
+			m_register.setStatus(current_line.op2, READ);
 			if (m_register.bge(current_line.op1, current_line.op2)) {
 				input.jumpLine(current_line.op3);
 			}
 			break;
 		case CALL:
-
+			break;
+		case RET:
+		case DRAW:
+		case END:
+		case TYPE_SIZE:
 		default:
 			break;
 		}
