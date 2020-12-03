dd262b64c2d6f22ca2fdf453f36a2ae551361b06
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-03 21:16:42 +0800

    增加注释

--- ERISC/input.cpp
+++ ERISC/input.cpp
@@ -2,6 +2,10 @@
 #include <fstream>
 #include <iostream>
 
+/**
+* @brief 去除字符串的首尾空格
+* @param str: 需要去除首尾空格的字符串
+*/
 static void trim(std::string& str) {
 	if (str.empty()) {
 		return;
@@ -9,11 +13,16 @@ static void trim(std::string& str) {
 	while (!str.empty() && *str.begin() == ' ') {
 		str.erase(str.begin());
 	}
-	while (!str.empty() && *str.end() == ' ') {
-		str.erase(str.end());
+	while (!str.empty() && *(str.end() - 1) == ' ') {
+		str.erase(str.end() - 1);
 	}
 }
 
+/**
+* @brief 把字符串以空格或逗号分割为向量
+* @param str: 待分割的字符串
+* @return 分割的字符串向量
+*/
 static std::vector<std::string> split(const std::string& str) {
 	if (str.empty()) {
 		return std::vector<std::string>();
@@ -21,7 +30,7 @@ static std::vector<std::string> split(const std::string& str) {
 	std::string s{};
 	std::vector<std::string> ss{};
 	for (auto iter = str.cbegin(); iter != str.cend(); ++iter) {
-		if (*iter != ' ') {
+		if (*iter != ' ' && *iter != ',') {
 			s.insert(s.end(), *iter);
 		}
 		else {
@@ -29,9 +38,14 @@ static std::vector<std::string> split(const std::string& str) {
 			s.clear();
 		}
 	}
+	ss.push_back(s);
 	return ss;
 }
 
+/**
+* @brief: 把str转化为大写
+* @param: str: 待转化的字符串
+*/
 static void toUpper(std::string& str) {
 	for (auto iter = str.begin(); iter != str.end(); ++iter) {
 		*iter = toupper(*iter);
@@ -54,7 +68,6 @@ Input::Input(std::string filename) {
 		std::getline(in, line);
 		trim(line);
 		if (!line.empty()) {
-			m_current_index++;
 			if (line[line.size() - 1] == ':') { //结尾为:,行标识符或者函数
 				std::string s{};
 				for (int i = 0; i <= line.size() - 2; i++) {
@@ -88,18 +101,14 @@ Input::Input(std::string filename) {
 				}
 				else if (a1 == "PUSH") {
 					std::string a2 = split_result[1];
-					std::string a3 = split_result[2];
 					line_struct.type = PUSH;
 					line_struct.op1 = a2;
-					line_struct.op2 = a3;
 					m_lines.push_back(line_struct);
 				}
 				else if (a1 == "POP") {
 					std::string a2 = split_result[1];
-					std::string a3 = split_result[2];
 					line_struct.type = POP;
 					line_struct.op1 = a2;
-					line_struct.op2 = a3;
 					m_lines.push_back(line_struct);
 				}
 				else if (a1 == "MOV") {
@@ -255,8 +264,10 @@ Input::Input(std::string filename) {
 				}
 			}
 		}
+		m_current_index++;
 	}
 	in.close();
+	m_current_index = 0;
 }
 
 /**
