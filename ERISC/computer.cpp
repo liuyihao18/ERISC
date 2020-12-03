a089287f31d3f62a20aff29261deb741284b889d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-03 19:46:52 +0800

    关闭流同步

--- ERISC/computer.cpp
+++ ERISC/computer.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "computer.h"
 #include <cstdio>
 #include <iostream>
--- ERISC/main.cpp
+++ ERISC/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+	ios::sync_with_stdio(false);
 	cout << "请输入你要运行的ERISC文件：" << endl;
 	string filename;
 	cin >> filename;
