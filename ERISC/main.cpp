4adfd03fb7ab45291d7f3277a2f64a622498dabd
怡豪 刘 <1163122901@qq.com>
2020-12-10 10:12:50 +0800

    加入程序计时

--- ERISC/main.cpp
+++ ERISC/main.cpp
@@ -1,13 +1,17 @@
 #include "computer.h"
+#include <ctime>
 #include <iostream>
 using namespace std;
 
 int main() {
 	ios::sync_with_stdio(false);
-	cout << "请输入你要运行的ERISC文件：" << endl;
+	cout << "请输入你要运行的ERISC文件：";
 	string filename;
 	cin >> filename;
 	Computer computer(filename);
+	auto begin = clock();
 	computer.main();
+	auto end = clock();
+	cout << "程序运行时间：" << (end - begin) / 1000 << "s" << endl;
 	return 0;
 }
